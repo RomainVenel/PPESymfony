89eac2a551eeb43929f04dbc81cee53adf50f47c
MaximeGenevier <maxime.genevier.sio@gmail.com>
2017-03-04 19:22:32 +0100

    Nettoyage du code, ajout des fonctionnalités des boutons et commentaires

--- src/rvmg/GSBBundle/Controller/AccueilController.php
+++ src/rvmg/GSBBundle/Controller/AccueilController.php
@@ -10,9 +10,17 @@ use rvmg\GSBBundle\Formulaires\Type\ConnexionFormType;
 
 class AccueilController extends Controller{
     
+    /**
+     * 
+     * @return type
+     * 
+     * Function that allows the connexion of the user
+     */
     public function connexionAccueilAction(){
         
+        //Initiate the ConnexionClass
         $connexion = new ConnexionClass();
+        //Create the form
         $form = $this->createForm(new ConnexionformType(),$connexion);        
         
         $request = $this->container->get('request');
@@ -20,17 +28,33 @@ class AccueilController extends Controller{
         $form->handleRequest($request);
         $this->getRequest()->getSession()->clear();
         
+        //IF the form is valid
         if ($form->isValid()){
+            
+            //Get the form's data
             $data = $form->getData();
+            
+            //Get Doctrine Manager
             $em = $this->getDoctrine()->getManager();
+            
+            //SWITCH profil
             switch($connexion->getProfil()){
+                
+                //VISITEUR
                 case 'Visiteur':
+                    
+                    //Get the repository and try to find the visiteur with the params past in the form
                     $repository = $em->getRepository('rvmgGSBBundle:Visiteur');
                     $visiteur = $repository->findByMdpAndLogin($connexion->getLogin(), $connexion->getMdp());
                     
+                    //IF the visitor is empty
                     if(!$visiteur){
+                        //Redirect the user to the error view
                         return $this->render('rvmgGSBBundle:Accueil:vueConnexionErreur.html.twig', array('data'=>$data));
-                    }else{
+                    }
+                    //ELSE
+                    else{
+                        //Create the session with the data we need 
                         $session = $request->getSession();
                         $session->set('user_login', $visiteur->getLogin());
                         $session->set('user_prenom', $visiteur->getPrenom());
@@ -39,17 +63,30 @@ class AccueilController extends Controller{
                         $session->set('user_firstname', $visiteur->getPrenom());
                         $session->set('user_lastname', $visiteur->getNom());
                         $session->set('user_id', $visiteur->getIdvisiteur());
+                        //Update the value 'connecte' to true
                         $repository->updateConnecteToTrue($connexion->getLogin());
               
+                        //Return the welcome page
                         return $this->render('rvmgGSBBundle:Visiteur:accueilVisiteur.html.twig');
                     }
                     break;
+                    
+                //COMPTABLE
                 case 'Comptable':
+                    
+                    //Get the repository and try to find the comptable with the params past in the form
                     $repository = $em->getRepository('rvmgGSBBundle:Comptable');
                     $comptable = $repository->findByMdpAndLogin($connexion->getLogin(), $connexion->getMdp());
+                    
+                    //IF the comptable is empty
                     if(!$comptable){
+                        //Redirect user to error page
                         return $this->render('rvmgGSBBundle:Accueil:vueConnexionErreur.html.twig', array('data'=>$data));
-                    }else{
+                    }
+                    //ELSE
+                    else{
+                        
+                        //Create the session with the data we need 
                         $session = $request->getSession();
                         $session->set('user_login', $comptable->getLogin());
                         $session->set('user_prenom', $comptable->getPrenom());
@@ -58,37 +95,57 @@ class AccueilController extends Controller{
                         $session->set('user_firstname', $comptable->getPrenom());
                         $session->set('user_lastname', $comptable->getNom());
                         $session->set('user_id', $comptable->getIdcomptable());
+                        //Update the value connecte to true
                         $repository->updateConnecteToTrue($connexion->getLogin());
                         
+                        //Redirect the user on the welcome page
                         return $this->render('rvmgGSBBundle:Comptable:accueilComptable.html.twig');
+                        
                     }
+                    
                     break;
+                
+                //DEFAULT
                 default:
+                    
                     echo 'Profil inexistant';
                     break;
             }
             
-            return $this->render('rvmgGSBBundle:Accueil:vueConnexionComptable.html.twig', array('data'=>$data));
         }
         
+        //Render the form for connexion function        
         return $this->render('rvmgGSBBundle:Accueil:index.html.twig', array('form'=>$form->createView()));
     }
     
+    /**
+     * 
+     * @return type
+     * 
+     * Function that disconnects the user
+     */
     public function deconnexionAction(){
         
+        //Get Doctrine Manager
         $em = $this->getDoctrine()->getManager();
 
+        //Get the login
         $profil = $this->getRequest()->getSession()->get('user_login');
         
+        //Test the profil to find the repository
         if ($this->getRequest()->getSession()->get('user_profil') == 'Visiteur'){
             $repository = $em->getRepository('rvmgGSBBundle:Visiteur');
         }else{
             $repository = $em->getRepository('rvmgGSBBundle:Comptable');
         }
+        
+        //Update the value "connecte" in the database to false
         $repository->updateConnecteToFalse($profil);
         
+        //Clear the session
         $this->getRequest()->getSession()->clear();
         
+        //Render the view disconnected
         return $this->render('rvmgGSBBundle:Accueil:vueDeconnexion.html.twig');
     }
 }
